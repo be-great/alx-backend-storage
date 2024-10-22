3483d59d404fad98b827ee372f3defb52b4f5a14
be-great <begreat5333@gmail.com>
2024-10-22 21:55:20 +0300

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
-"""change school topics"""
+"""12. Log stats"""
+from pymongo import MongoClient
+
 
 def update_topics(mongo_collection, name, topics):
     """change school topics"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}});
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
+
+def run():
+    "prime code"
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    print('{} logs'.format(collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        count = len(list(collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, count))
+    # count get=> /status path
+    count_get_status = len(list(
+        collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(count_get_status))
+
+
+if __name__ == '__main__':
+    run()
